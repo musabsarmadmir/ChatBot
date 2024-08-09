117bbdd208ed309541eb7cd221f2f3803ca1990d
Nareen Asad <bennynanny80@gmail.com>
2024-08-10 04:18:31 +0500

    changes

--- chatsupport/app/page.js
+++ chatsupport/app/page.js
@@ -93,10 +93,10 @@ export default function Home() {
       addMessage(1, "Hello! I'm Kazir AI. How can I assist you today?", true);
     }
     if (messages.length > 0 && !messages[messages.length - 1].isBot) {
-      sendToLlama(messages.length, inputValue.trim(), addMessage);
+      sendToLlama(messages.length);
       setInputValue("");
     }
-  }, [messages, addMessage, inputValue]);
+  }, [messages]);
 
   useEffect(() => {
     if (chatBoxBodyRef.current) {
