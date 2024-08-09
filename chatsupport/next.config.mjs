0850d950080fec1543da281d2b3f6e59a028c1e8
megane18 <meganealexis12@gmail.com>
2024-08-09 16:22:30 -0400

    Fixing bug and removing package.json

--- chatsupport/next.config.mjs
+++ chatsupport/next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+      NEXT_PUBLIC_GROQ_API_KEY: process.env.NEXT_PUBLIC_GROQ_API_KEY,
+    },
+    // Add any other configuration options here
+  };
+  
+  export default nextConfig;
