608583d0b2728d1b9ccf09bc39206225239684e7
megane18 <meganealexis12@gmail.com>
2024-08-09 16:09:33 -0400

    Added environment variable

--- chatsupport/app/api/chat/route.js
+++ chatsupport/app/api/chat/route.js
@@ -1,14 +1,14 @@
 import Groq from "groq-sdk";
 
-const groq = new Groq({ apiKey: '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' , dangerouslyAllowBrowser: true});
+const groq = new Groq({ 
+  apiKey: process.env.NEXT_PUBLIC_GROQ_API_KEY, 
+  dangerouslyAllowBrowser: true 
+});
 
-
-export async function sendToLlama(messagesLength,clientMessage,addMessageReference) {
-
+export async function sendToLlama(messagesLength, clientMessage, addMessageReference) {
   const chatCompletion = await getGroqChatCompletion(clientMessage);
-  // Print the completion returned by the LLM.
   console.log(chatCompletion.choices[0]?.message?.content || "");
-  addMessageReference(messagesLength+1,chatCompletion.choices[0]?.message?.content || "",true);
+  addMessageReference(messagesLength + 1, chatCompletion.choices[0]?.message?.content || "", true);
 }
 
 export async function getGroqChatCompletion(data) {
@@ -17,7 +17,7 @@ export async function getGroqChatCompletion(data) {
     messages: [
       {
         role: "user",
-        content: data
+        content: data,
       },
     ],
     model: "llama3-8b-8192",
