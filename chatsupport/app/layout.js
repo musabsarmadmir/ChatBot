3bf24cb6748c2267c31c3b66146b8e3df4f3a184
Megane Alexis <meganealexis12@gmail.com>
2024-08-10 12:19:20 -0400

    Added .env file

--- chatsupport/app/layout.js
+++ chatsupport/app/layout.js
@@ -4,8 +4,15 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "AI Customer Support",
+  title: "AI# Customer Support",
   description: "To resolve your queries, we have developed an AI Customer Support system.",
+};
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: 'no'
 };
 
 export default function RootLayout({ children }) {
@@ -13,7 +20,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         {children}
-        </body>
+      </body>
     </html>
   );
 }
